ce4e87ead70d7b3dba0afc205db6d53e27999c5d
Jenny Lindgren <jema.lindgren@gmail.com>
2023-03-15 14:16:51 +0100

    final clean up

--- src/products.js
+++ src/products.js
@@ -19,78 +19,78 @@ export const PRODUCTS = [
     {
       id: 1,
       productName: "Samsung Galaxy Flip",
-      price: 999.0,
+      price: 4999.0,
       productImage: product1,
-      text:"galaxy Flip"
+      text:"Samsung Galaxy Z Flip4-smarttelefon tar den unika vikbara designen till ännu högre nivåer."
     },
     {
       id: 2,
       productName: "Iphone 13",
-      price: 1999.0,
+      price: 7999.0,
       productImage: product2,
-      text:"Iphone 13 ProMax, en schysst telefon"
+      text:"Nya iPhone 13 smartphone har en förbättrad dubbel 12Mpx-kamera med sensor-shift OIS och A15 Bionic-processor med 5G-stöd och kraftfulla AI-funktioner."
     },
     {
       id: 3,
       productName: "Iphone 14 Plus",
-      price: 699.0,
+      price: 12699.0,
       productImage: product3,
-      text:"lalala"
+      text:"iPhone 14 Plus-smartphone är utrustad med en fantastisk dubbel 12 Mpx-kamerauppsättning med Photonic Engine, kraftfull A15 Bionic-processor och 5G-dataanslutning."
     },
     {
       id: 4,
       productName: "Iphone 12 minit",
-      price: 228.0,
+      price: 8228.0,
       productImage: product4,
-      text:"lalalalla"
+      text:"Med iPhone 13 mini smartphone får du kraftfulla funktioner i fickstorlek."
     },
     {
       id: 5,
       productName: "Iphone 13 Pro",
-      price: 19.99,
+      price: 3119.99,
       productImage: product5,
-      text:"lalalla"
+      text:"Avancerade kamerasystem och briljanta skärmar. Tålig design. Snabba, smarta och strömsnåla chip."
     },
     {
       id: 6,
       productName: "Iphone XR",
-      price: 68.0,
+      price: 2900.0,
       productImage: product6,
-      text:"lalalallaa"
+      text:"lAvancerade kamerasystem och briljanta skärmar. Tålig design. Snabba, smarta och strömsnåla chip."
     },
     {
       id: 7,
       productName: "Iphone 12",
-      price: 120.0,
+      price: 7000.0,
       productImage: product7,
-      text:"lalalalla"
+      text:"Spänn fast dig för den häpnadsväckande hastigheten hos iPhone 12 smartphone. Ta fantastiska foton och videor i hög upplösning med 12Mpx Dual-kamerasystem."
     },
     {
       id: 8,
       productName: "Samsung galaxy a23",
-      price: 40.0,
+      price: 3200.0,
       productImage: product8,
-      text:"lalallala"
+      text:"Samsung Galaxy A23 5G smartphone kommer med en stor 6.6 FHD+-skärm med en snabb 120Hz-uppdateringsfrekvens och vackra färger"
     },
     {
         id: 9,
         productName: "Samsung galaxy s23",
-        price: 40.0,
+        price: 10033.0,
         productImage: product9,
-        text:"lalalla"
+        text:"Samsung Galaxy S23 5G smartphone har en elegant och modern design, fullspäckad med högklassig teknologi."
       },
       {
          id: 10,
         productName: "Samsung S20",
-        price: 40.0,
+        price: 7200.0,
         productImage: product10,
-        text:"lalalala"
+        text:"Samsung Galaxy S20 FE 5G är en smartphone och multimediaenhet som kombinerar kraftfull prestanda med AI bildprocess för proffesionella bilder"
       },
       {
         id: 11,
-         productName: "Samsung FE",
-        price: 40.0,
+         productName: "Samsung S21 FE",
+        price: 7980.0,
         productImage: product11,
-        text:"lalalal"
+        text:"Samsung Galaxy S21FE smartphone är en fantastisk daglig följeslagare med en stor 6,4-tums pekskärm och ett kraftfullt 4500mAh batteri."
       },
   ];
